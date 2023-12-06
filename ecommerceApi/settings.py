ce2a45fd51ac1fd93df486fd690c0f47b75acd73
rubikazzbassmrn <rubikazzbassmrn@gmail.com>
2023-12-06 00:26:26 -0600

    collectstatic

--- ecommerceApi/settings.py
+++ ecommerceApi/settings.py
@@ -141,6 +141,7 @@ USE_TZ = os.getenv('USE_TZ')
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
